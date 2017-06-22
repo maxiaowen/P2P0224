452e2e1f1532ec6e995a6ec670ac6487ad65d1ca
maxiaowen <332168460@qq.com>
2017-06-22 10:34:38 +0800

    解析数据

--- app/src/main/java/com/atguigu/p2p0224/fragment/HomeFragment.java
+++ app/src/main/java/com/atguigu/p2p0224/fragment/HomeFragment.java
@@ -4,18 +4,27 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.alibaba.fastjson.JSON;
 import com.atguigu.p2p0224.R;
+import com.atguigu.p2p0224.bean.IndexBean;
 import com.atguigu.p2p0224.common.AppNetConfig;
 import com.atguigu.p2p0224.utils.UIUtils;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.squareup.picasso.Picasso;
 import com.youth.banner.Banner;
 import com.youth.banner.loader.ImageLoader;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -65,7 +74,86 @@ public class HomeFragment extends Fragment {
     }
 
     private List<String> list = new ArrayList<>();
+
     private void initData() {
+        loadNet();
+        initBanner();
+    }
+
+    /**
+     * 联网请求
+     */
+    private void loadNet() {
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(AppNetConfig.INDEX, new AsyncHttpResponseHandler(){
+
+            @Override
+            public void onSuccess(int statusCode, String content) {
+                super.onSuccess(statusCode, content);
+                Log.e("TAG", "请求成功: "+content);
+
+                //手动解析
+//                try {
+//                    parseJson(content);
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+
+                IndexBean indexBean = JSON.parseObject(content,IndexBean.class);
+                Log.e("TAG", "onSuccess: "+indexBean.getProInfo().getName());
+
+            }
+
+            @Override
+            public void onFailure(Throwable error, String content) {
+                super.onFailure(error, content);
+                Log.e("TAG", "请求失败: "+content);
+            }
+        });
+    }
+
+    /**
+     * 手动解析数据
+     *
+     * @param json
+     */
+    private void parseJson(String json) throws JSONException {
+
+        //创建IndexBean对象
+        IndexBean bean = new IndexBean();
+        //创建一个集合
+        List<IndexBean.ImageArrBean> list = new ArrayList<>();
+        //解析最外层的对象
+        JSONObject object = new JSONObject(json);
+        //获取对象中的第一个元素
+        JSONArray imageArr = object.getJSONArray("imageArr");
+        for (int i = 0; i < imageArr.length(); i++) {
+            //创建一个集合里面的子元素
+            IndexBean.ImageArrBean imageArrBean = new IndexBean.ImageArrBean();
+            //获取子元素数组中的对象
+            JSONObject jsonObject = imageArr.getJSONObject(i);
+            //解析数组中对象的元素
+            String id = jsonObject.getString("ID");
+            imageArrBean.setID(id);
+            String imapaurl = jsonObject.getString("IMAPAURL");
+            imageArrBean.setIMAPAURL(imapaurl);
+            String imaurl = jsonObject.getString("IMAURL");
+            imageArrBean.setIMAURL(imaurl);
+            //把数组元素的对象添加到集合中
+            list.add(imageArrBean);
+        }
+
+        bean.setImageArr(list);
+
+        //获取对象中的第二个元素
+        JSONObject proInfo = object.getJSONObject("proInfo");
+        String name = proInfo.getString("name");
+        int id = proInfo.getInt("id");
+        Log.d("json", "parseJson: "+name);
+
+    }
+
+    private void initBanner() {
         list.add(AppNetConfig.BASE_URL + "images/index02.png");
 //        Log.e("TAG",AppNetConfig.BASE_URL + "images/index02.png");
         //设置图片加载器
@@ -74,14 +162,13 @@ public class HomeFragment extends Fragment {
         banner.setImages(list);
         //启动banner
         banner.start();
-
     }
 
     /*
    * 加载图片类
    *
    * */
-    class GlideImageLoader extends ImageLoader{
+    class GlideImageLoader extends ImageLoader {
         @Override
         public void displayImage(Context context, Object path, ImageView imageView) {
             Picasso.with(context).load((String) path).into(imageView);
