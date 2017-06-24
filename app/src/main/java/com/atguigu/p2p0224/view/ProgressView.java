a83157c92f902b28779a443a559ae722ca85826b
maxiaowen <332168460@qq.com>
2017-06-24 11:09:14 +0800

    自定义属性

--- app/src/main/java/com/atguigu/p2p0224/view/ProgressView.java
+++ app/src/main/java/com/atguigu/p2p0224/view/ProgressView.java
@@ -2,6 +2,7 @@ package com.atguigu.p2p0224.view;
 
 import android.animation.ValueAnimator;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -10,6 +11,7 @@ import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 
+import com.atguigu.p2p0224.R;
 import com.atguigu.p2p0224.utils.UIUtils;
 
 /**
@@ -24,15 +26,37 @@ public class ProgressView extends View {
     private Paint paint;
     private int sweepAngle = 0;
 
+    private int paintColor;
+    private int textColor;
+    private int arcColor;
+
 
     public ProgressView(Context context) {
         super(context);
         init();
     }
 
+    /*
+    * 自定义属性 三步
+    * 第一步 创建attrs文件
+    * 第二步 在自定义控件构造器方法中 实例化attrs对象并获取属性名称和默认值
+    * 第三步 在布局文件中使用自定义的属性
+    * */
     public ProgressView(Context context, AttributeSet attrs) {
         super(context, attrs);
         init();
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.ProgressView);
+        int color = typedArray.getColor(R.styleable.ProgressView_paintColor, Color.BLACK);
+        this.paintColor = color;
+        int tcolor = typedArray.getColor(R.styleable.ProgressView_textColor, Color.BLUE);
+        this.textColor = tcolor;
+
+        int arcColor = typedArray.getColor(R.styleable.ProgressView_arcColor, Color.RED);
+        this.arcColor = arcColor;
+
+        //释放资源文件
+        typedArray.recycle();
+
     }
 
     private void init() {
@@ -58,7 +82,7 @@ public class ProgressView extends View {
 
         //画圆
         paint.setStrokeWidth(strokeWidth);
-        paint.setColor(Color.BLACK);
+        paint.setColor(paintColor);
         int cx = width / 2;
         int cy = height / 2;
         int radius = cx - strokeWidth / 2;
@@ -66,13 +90,14 @@ public class ProgressView extends View {
 
 
         //花弧
-        paint.setColor(Color.RED);
+        paint.setColor(arcColor);
         RectF rectF = new RectF();
         rectF.set(strokeWidth / 2, strokeWidth / 2, width - strokeWidth / 2, height - strokeWidth / 2);
         canvas.drawArc(rectF, 0, sweepAngle, false, paint);
 
 
         //画文字
+        paint.setColor(textColor);
         paint.setStrokeWidth(0); //设置画笔的宽度
         String str = sweepAngle + "%";
         paint.setTextSize(30);//设置文字的大小
